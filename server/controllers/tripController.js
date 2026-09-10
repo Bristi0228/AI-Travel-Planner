0b0c08f9c9c57f7cc4cdfb6059b956035debfe25
Shreyan Dey <shreyandey1234@gmail.com>
2026-09-10 12:26:14 +0530

    add freature ai generated itenitinerary

--- server/controllers/tripController.js
+++ server/controllers/tripController.js
@@ -1,5 +1,16 @@
+import mongoose from 'mongoose';
+import OpenAi from 'openai';
+import { v4 as uuidv4 } from 'uuid';
+import Trip from '../models/Trip.js';
+
+
+const openai = new OpenAi({ apiKey: process.env.OPENAI_API_KEY });
+
 const generateTrip = async (req, res) => {
-    console.log("Received request to generate trip");
+    const { destination, inputs = {} } = req.body;
+
+    const duration = parseInt(inputs.duration) || 3;
+    const travellers = parseInt(inputs.numTravelers) || 1;
 };
 
 export default {
